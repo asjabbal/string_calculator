4127bfd242fcac26718be99115d3bddaf81ffeea
Amrinder Jabbal <amrinder.jabbal@gmail.com>
2025-05-23 22:27:11 +0530

    add multiple numbers string test case

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -9,4 +9,8 @@ class StringCalculatorTest < Minitest::Test
   def test_simple_numbers_str
     assert_equal 3, StringCalculator.new.add("1,2")
   end
+
+  def test_multiple_numbers_str
+    assert_equal 28, StringCalculator.new.add("1,2,3,4,5,6,7")
+  end
 end
