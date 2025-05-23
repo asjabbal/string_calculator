6701851bcaa258c9c18864c940a5ae778afb60b5
Amrinder Jabbal <amrinder.jabbal@gmail.com>
2025-05-23 22:49:51 +0530

    add logic to support negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,12 @@
 class StringCalculator
+  class NegativeNumbersNotAllowed < StandardError; end
+
   def add(numbers_str)
     numbers = extract_numbers(numbers_str)
+
+    negative_numbers = check_negative_numbers(numbers)
+    raise NegativeNumbersNotAllowed, "Negative numbers not allowed: #{negative_numbers}" if negative_numbers.size > 0
+
     numbers.sum
   end
 
@@ -9,5 +15,9 @@ class StringCalculator
   def extract_numbers(numbers_str)
     numbers = numbers_str.scan(/-?\d+/)
     numbers.map{|num| num.to_i}
+  end
+
+  def check_negative_numbers(numbers)
+    numbers.select{|num| num < 0}
   end
 end
