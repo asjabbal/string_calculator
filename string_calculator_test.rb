a66568b84283c0e04f7b9426fb9b443ad2348d1e
Amrinder Jabbal <amrinder.jabbal@gmail.com>
2025-05-23 22:18:02 +0530

    add initial tests

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -2,4 +2,11 @@ require 'minitest/autorun'
 require_relative 'string_calculator'
 
 class StringCalculatorTest < Minitest::Test
+  def test_blank_numbers_str
+    assert_equal 0, StringCalculator.new.add("")
+  end
+
+  def test_simple_numbers_str
+    assert_equal 3, StringCalculator.new.add("1,2")
+  end
 end
