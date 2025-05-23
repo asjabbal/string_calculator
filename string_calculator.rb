3219c75ce4043cd2cbbe0bd1e28a2a71cde45a10
Amrinder Jabbal <amrinder.jabbal@gmail.com>
2025-05-23 22:24:44 +0530

    add base logic to add numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,4 +1,13 @@
 class StringCalculator
   def add(numbers_str)
+    numbers = extract_numbers(numbers_str)
+    numbers.sum
+  end
+
+  private
+
+  def extract_numbers(numbers_str)
+    numbers = numbers_str.scan(/-?\d+/)
+    numbers.map{|num| num.to_i}
   end
 end
