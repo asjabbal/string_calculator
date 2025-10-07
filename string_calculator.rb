fb46331e376de884675465346d54f20c67995d10
Amrinder Jabbal <amrinder.jabbal@gmail.com>
2025-10-07 16:44:59 +0530

    Revert "cubes addition"
    This reverts commit bf0aa72a0aa3538aec9dd9e7a5f365e7e4ebe2dc.

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,7 +3,6 @@ class StringCalculator
 
   def add(numbers_str)
     numbers = extract_numbers(numbers_str)
-    numbers = check_cubes(numbers)
 
     negative_numbers = check_negative_numbers(numbers)
     raise NegativeNumbersNotAllowed, "Negative numbers not allowed: #{negative_numbers}" if negative_numbers.size > 0
@@ -34,24 +33,5 @@ class StringCalculator
 
   def reject_numbers_gt_1000(numbers)
     numbers.reject{|num| num > 1000}
-  end
-
-  def check_cubes(numbers)
-    numbers_count = {}
-    filtered_numbers = []
-    numbers.each do |n|
-      numbers_count[n] = numbers_count[n].to_i + 1
-      filtered_numbers << n if numbers_count[n] >= 3
-    end
-
-    if filtered_numbers.size == 0
-      return numbers
-    end
-    numbers = numbers - filtered_numbers
-
-    cubes_arr = filtered_numbers.map{|n| n**3}
-    numbers = numbers + cubes_arr
-
-    numbers
   end
 end
